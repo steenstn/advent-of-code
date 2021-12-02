0e58785427b2645b5e65d0f27e7d44fa8dd2cea0
steenstn <alexander.steen@qapital.com>
2021-12-02 09:55:21 +0100

    small fix

--- 2021/src/main/kotlin/Day.kt
+++ 2021/src/main/kotlin/Day.kt
@@ -4,6 +4,6 @@ abstract class Day(filePath: String) {
     val list: List<String>
 
     init {
-        list = File(object {}.javaClass.getResource(filePath).toURI()).readLines()
+        list = File(this.javaClass.getResource(filePath).toURI()).readLines()
     }
 }
