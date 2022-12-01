08bb851fb2a71092f413b1d9052f000d446d5460
steenstn <alexander.steen@qapital.com>
2022-12-01 07:06:49 +0100

    day1

--- 2021/src/main/kotlin/year2022/Day1.kt
+++ 2021/src/main/kotlin/year2022/Day1.kt
@@ -2,13 +2,30 @@ package year2022
 
 import Day
 
-class Day1: Day("/year2022/day1.txt") {
+class Day1 : Day("/year2022/day1.txt") {
+
+    val elves = mutableListOf<Int>()
+
+    init {
+        var sum = 0
+        list.forEach {
+            if (it.isBlank()) {
+                elves.add(sum)
+                sum = 0
+            } else {
+                sum += Integer.parseInt(it)
+            }
+        }
+
+    }
+
     fun part1() {
-        println(list)
+        println(elves.maxOf { it })
     }
 
     fun part2() {
-
+        val sorted = elves.sortedDescending()
+        println(sorted[0] + sorted[1] + sorted[2])
     }
 
 
---
+++ 2021/src/main/kotlin/year2022/Day2.kt
@@ -0,0 +1,18 @@
+package year2022
+
+import Day
+
+class Day2 : Day("/year2022/day2.txt") {
+
+    init {
+
+    }
+
+    fun part1() {
+
+    }
+
+    fun part2() {
+
+    }
+}
