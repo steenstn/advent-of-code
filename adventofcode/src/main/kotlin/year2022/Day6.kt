35efe9f5bce49a7ee266e05f242f7381a32f7ec6
steenstn <alexander.steen@qapital.com>
2022-12-06 06:44:04 +0100

    Day6

--- adventofcode/src/main/kotlin/year2022/Day6.kt
+++ adventofcode/src/main/kotlin/year2022/Day6.kt
@@ -4,15 +4,29 @@ import Day
 
 class Day6 : Day("/year2022/day6.txt") {
 
-    init {
-
-    }
-
     fun part1() {
+        var index = 0
+        var done = false
+        list.single().windowed(size = 4, step = 1).forEach {
+            index++
 
+            if (it.toSet().size == 4 && !done) {
+                println(index + 3)
+                done = true
+            }
+        }
     }
 
     fun part2() {
+        var index = 0
+        var done = false
+        list.single().windowed(size = 14, step = 1).forEach {
+            index++
 
+            if (it.toSet().size == 14 && !done) {
+                println(index + 13)
+                done = true
+            }
+        }
     }
 }
---
+++ adventofcode/src/main/kotlin/year2022/Day7.kt
@@ -0,0 +1,18 @@
+package year2022
+
+import Day
+
+class Day7 : Day("/year2022/day7.txt") {
+
+    init {
+
+    }
+
+    fun part1() {
+
+    }
+
+    fun part2() {
+
+    }
+}
