d5b430c5d916738717ff7d20db47b94a2667e74a
Alexander Steen <alexander.steen@qapital.com>
2023-12-04 21:12:31 +0100

    A bit nicer

--- adventofcode/src/main/kotlin/year2016/Day3.kt
+++ adventofcode/src/main/kotlin/year2016/Day3.kt
@@ -11,7 +11,7 @@ class Day3 : Day() {
             val a = sides[0]
             val b = sides[1]
             val c = sides[2]
-            if(a+b>c && a+c > b && b+c > a) {
+            if (a + b > c && a + c > b && b + c > a) {
                 sum++
             }
         }
@@ -21,26 +21,13 @@ class Day3 : Day() {
     fun part2() {
         var sum = 0
         list.chunked(3).forEach { group: List<String> ->
-            val t1a = group[0].trim().split(Regex("\\s+"))[0].toInt()
-            val t1b = group[1].trim().split(Regex("\\s+"))[0].toInt()
-            val t1c = group[2].trim().split(Regex("\\s+"))[0].toInt()
-
-            val t2a = group[0].trim().split(Regex("\\s+"))[1].toInt()
-            val t2b = group[1].trim().split(Regex("\\s+"))[1].toInt()
-            val t2c = group[2].trim().split(Regex("\\s+"))[1].toInt()
-
-            val t3a = group[0].trim().split(Regex("\\s+"))[2].toInt()
-            val t3b = group[1].trim().split(Regex("\\s+"))[2].toInt()
-            val t3c = group[2].trim().split(Regex("\\s+"))[2].toInt()
-
-            if(t1a+t1b>t1c && t1a+t1c > t1b && t1b+t1c > t1a) {
-                sum++
-            }
-            if(t2a+t2b>t2c && t2a+t2c > t2b && t2b+t2c > t2a) {
-                sum++
-            }
-            if(t3a+t3b>t3c && t3a+t3c > t3b && t3b+t3c > t3a) {
-                sum++
+            for (i in 0..2) {
+                val a = group[0].trim().split(Regex("\\s+"))[i].toInt()
+                val b = group[1].trim().split(Regex("\\s+"))[i].toInt()
+                val c = group[2].trim().split(Regex("\\s+"))[i].toInt()
+                if (a + b > c && a + c > b && b + c > a) {
+                    sum++
+                }
             }
         }
         println(sum)
