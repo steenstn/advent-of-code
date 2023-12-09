dcd37ff2386f6bf4bf76ffd15404d9541fd676da
Alexander Steen <alexander.steen@qapital.com>
2023-12-09 14:33:56 +0100

    day9 part1

--- adventofcode/src/main/kotlin/Main.kt
+++ adventofcode/src/main/kotlin/Main.kt
@@ -1,9 +1,8 @@
-import year2023.Day7
-import year2023.Day8
+import year2023.Day9
 import kotlin.time.measureTime
 
 fun main() {
-   val time = measureTime { Day8().part2() }
+   val time = measureTime { Day9().part1() }
    println()
    println("Duration: ${time.inWholeMilliseconds} ms")
 
--- adventofcode/src/main/kotlin/year2023/Day9.kt
+++ adventofcode/src/main/kotlin/year2023/Day9.kt
@@ -4,5 +4,36 @@ import Day
 
 class Day9 : Day() {
 
-    
+    fun part1() {
+        var historySum = 0L
+
+        list.forEach { line ->
+            val numbers = line.split(" ").map { it.toInt() }.toMutableList()
+
+            val differences = mutableListOf<MutableList<Int>>()
+            differences.add(getDifference(numbers))
+            while (true) {
+                if (differences.last().all { it == 0 }) {
+                    break
+                }
+                differences.add(getDifference(differences.last()))
+            }
+            differences.last().add(0)
+            for (i in differences.size - 2 downTo 0) {
+                differences[i].add(differences[i].last() + differences[i + 1].last())
+            }
+            numbers.add(numbers.last() + differences[0].last())
+            historySum += numbers.last()
+        }
+        println(historySum)
+    }
+
+    private fun getDifference(numbers: List<Int>): MutableList<Int> {
+        val result = mutableListOf<Int>()
+        for (i in 1..<numbers.size) {
+            result.add(numbers[i] - numbers[i - 1])
+        }
+        return result
+    }
+
 }
