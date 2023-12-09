5948e9af5c523c06ee3714edd6f78ee6a276da58
Alexander Steen <alexander.steen@qapital.com>
2023-12-09 08:49:28 +0100

    Day5. Not smart but works

--- adventofcode/src/main/kotlin/year2023/Day5.kt
+++ adventofcode/src/main/kotlin/year2023/Day5.kt
@@ -6,21 +6,50 @@ class Day5 : Day() {
 
     data class Range(val source: LongRange, val destination: LongRange)
 
+    private val seedToSoilMap = parseMap("seed-to-soil")
+    private val soilToFertilizerMap = parseMap("soil-to-fertilizer")
+    private val fertilizerToWaterMap = parseMap("fertilizer-to-water")
+    private val waterToLightMap = parseMap("water-to-light")
+    private val lightToTemperatureMap = parseMap("light-to-temperature")
+    private val temperatureToHumidityMap = parseMap("temperature-to-humidity")
+    private val humidityToLocationMap = parseMap("humidity-to-location")
+
     fun part1() {
         val seeds = list.first().substringAfter(":").trim().split(Regex("\\s+")).map { it.toLong() }
-        val seedToSoilMap = parseMap("seed-to-soil")
-        val soilToFertilizerMap = parseMap("soil-to-fertilizer")
-        val fertilizerToWaterMap = parseMap("fertilizer-to-water")
-        val waterToLightMap = parseMap("water-to-light")
-        val lightToTemperatureMap = parseMap("light-to-temperature")
-        val temperatureToHumidityMap = parseMap("temperature-to-humidity")
-        val humidityToLocationMap = parseMap("humidity-to-location")
-
-        val minLocation = seeds.map { moveSeed(moveSeed(moveSeed(moveSeed(moveSeed(moveSeed(moveSeed(it, seedToSoilMap), soilToFertilizerMap), fertilizerToWaterMap), waterToLightMap), lightToTemperatureMap),temperatureToHumidityMap),humidityToLocationMap) }.min()
+        val minLocation = seeds.map {
+            moveSeed(
+                moveSeed(
+                    moveSeed(moveSeed(moveSeed(moveSeed(moveSeed(it, seedToSoilMap), soilToFertilizerMap), fertilizerToWaterMap), waterToLightMap), lightToTemperatureMap),
+                    temperatureToHumidityMap
+                ), humidityToLocationMap
+            )
+        }.min()
         println(minLocation)
 
     }
 
+
+    fun part2() {
+        val seeds = list.first().substringAfter(":").trim().split(Regex("\\s+")).chunked(2).map { LongRange(it.first().toLong(), it.first().toLong() + it.last().toLong() - 1) }
+        var min = Long.MAX_VALUE
+        seeds.forEachIndexed { index, seedRange ->
+            println("$index of ${seeds.size}")
+            seedRange.forEach {
+                val endResult = moveSeed(
+                    moveSeed(
+                        moveSeed(moveSeed(moveSeed(moveSeed(moveSeed(it, seedToSoilMap), soilToFertilizerMap), fertilizerToWaterMap), waterToLightMap), lightToTemperatureMap),
+                        temperatureToHumidityMap
+                    ), humidityToLocationMap
+                )
+                if (endResult < min) {
+                    min = endResult
+                }
+            }
+
+        }
+        println(min)
+    }
+    
     private fun moveSeed(seed: Long, rangeMap: List<Range>): Long {
         val sourceRange: Range = rangeMap.find { it.source.contains(seed) } ?: return seed
         val positionInRange = seed - sourceRange.source.first
@@ -31,11 +60,7 @@ class Day5 : Day() {
         val numbers = list.dropWhile { !it.contains(mapName) }
             .drop(1)
             .takeWhile { it.contains(Regex("\\d")) }
-        val resultingRanges = numbers.map { line -> line.split(Regex("\\s+")).map { it.toLong() } }.map { Range(LongRange(it[1], it[1] + it[2]-1), LongRange(it[0], it[0] + it[2]-1)) }
+        val resultingRanges = numbers.map { line -> line.split(Regex("\\s+")).map { it.toLong() } }.map { Range(LongRange(it[1], it[1] + it[2] - 1), LongRange(it[0], it[0] + it[2] - 1)) }
         return resultingRanges
-    }
-
-    fun part2() {
-
     }
 }
